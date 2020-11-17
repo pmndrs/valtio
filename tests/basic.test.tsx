84cd5179cac7264a86e4ce3ecc1005ca6c82b3ff
daishi <daishi@axlight.com>
2020-11-17 22:21:17 +0900

    add failing test

--- tests/basic.test.tsx
+++ tests/basic.test.tsx
@@ -138,3 +138,30 @@ it('array in object', async () => {
   fireEvent.click(getByText('button'))
   await findByText('counts: 0,1,2,3')
 })
+
+it.skip('circular object', async () => {
+  const obj = create<any>({ object: {} })
+  obj.object = obj
+  obj.object.count = 0
+
+  const Counter: React.FC = () => {
+    const snapshot = useProxy(obj)
+    return (
+      <>
+        <div>count: {snapshot.count}</div>
+        <button onClick={() => ++obj.count}>button</button>
+      </>
+    )
+  }
+
+  const { getByText, findByText } = render(
+    <StrictMode>
+      <Counter />
+    </StrictMode>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('button'))
+  await findByText('count: 1')
+})
