3be76e1000b7539bf40433b502cc16c0ea677134
daishi <daishi@axlight.com>
2020-11-23 12:05:49 +0900

    fix: snapshots have to be plain objects

--- tests/class.test.tsx
+++ tests/class.test.tsx
@@ -102,3 +102,133 @@ it('no extra re-renders with class', async () => {
     getByText('count2: 1 (1)')
   })
 })
+
+it('inherited class without methods', async () => {
+  class BaseClass {
+    public count: number
+    constructor() {
+      this.count = 0
+    }
+  }
+  class CountClass extends BaseClass {
+    public count2: number
+    constructor() {
+      super()
+      this.count2 = 0
+    }
+  }
+
+  const obj = proxy(new CountClass())
+
+  const Counter: React.FC = () => {
+    const snapshot = useProxy(obj)
+    return (
+      <>
+        <div>count: {snapshot.count}</div>
+        <button onClick={() => ++obj.count}>button</button>
+      </>
+    )
+  }
+
+  const { getByText, findByText } = render(
+    <StrictMode>
+      <Counter />
+    </StrictMode>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('button'))
+  await findByText('count: 1')
+})
+
+it('class with a class', async () => {
+  class CountClass {
+    public count: number
+    constructor() {
+      this.count = 0
+    }
+    public doubled() {
+      return this.count * 2
+    }
+  }
+
+  const obj = proxy(new CountClass())
+
+  const Counter: React.FC = () => {
+    const snapshot = useProxy(obj)
+    return (
+      <>
+        <div>count: {snapshot.count}</div>
+        <div>doubled: {snapshot.doubled()}</div>
+        <button onClick={() => ++obj.count}>button</button>
+      </>
+    )
+  }
+
+  const { getByText } = render(
+    <StrictMode>
+      <Counter />
+    </StrictMode>
+  )
+
+  await waitFor(() => {
+    getByText('count: 0')
+    getByText('doubled: 0')
+  })
+
+  fireEvent.click(getByText('button'))
+  await waitFor(() => {
+    getByText('count: 1')
+    getByText('doubled: 2')
+  })
+})
+
+it('inherited class with a method', async () => {
+  class BaseClass {
+    public count: number
+    constructor() {
+      this.count = 0
+    }
+    public doubled() {
+      return this.count * 2
+    }
+  }
+  class CountClass extends BaseClass {
+    public count2: number
+    constructor() {
+      super()
+      this.count2 = 0
+    }
+  }
+
+  const obj = proxy(new CountClass())
+
+  const Counter: React.FC = () => {
+    const snapshot = useProxy(obj)
+    return (
+      <>
+        <div>count: {snapshot.count}</div>
+        <div>doubled: {snapshot.doubled()}</div>
+        <button onClick={() => ++obj.count}>button</button>
+      </>
+    )
+  }
+
+  const { getByText } = render(
+    <StrictMode>
+      <Counter />
+    </StrictMode>
+  )
+
+  await waitFor(() => {
+    getByText('count: 0')
+    getByText('doubled: 0')
+  })
+
+  fireEvent.click(getByText('button'))
+  await waitFor(() => {
+    getByText('count: 1')
+    getByText('doubled: 2')
+  })
+})
