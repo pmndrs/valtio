d5d1d8504975bb92a89cecddb829595c64c6ce86
c0d3t3k <73602268+c0d3t3k@users.noreply.github.com>
2020-12-07 04:28:31 -0500

    Add subscribeKey Utility (#40)
    * Add UseSubscribeKey
    
    * Add subscribeKey
    
    * proper typing and so on
    
    * chore: rename to value
    
    Co-authored-by: c0d3t3k <>
    Co-authored-by: daishi <daishi@axlight.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { useRef } from 'react'
-import { proxy, useProxy } from 'valtio'
+import { proxy, useProxy, subscribe } from 'valtio'
 
 /**
  * useLocalProxy
@@ -22,4 +22,28 @@ export const useLocalProxy = <T extends object>(init: T | (() => T)) => {
     ref.current = proxy(initialObject)
   }
   return [useProxy(ref.current), ref.current] as const
+}
+
+/**
+ * subscribeKey
+ *
+ * The subscribeKey utility enables subscription to a primitive subproperty of a given state proxy.
+ * Subscriptions created with subscribeKey will only fire when the specified property changes.
+ *
+ * @example
+ * import { subscribeKey } from 'valtio/utils'
+ * subscribeKey(state, 'count', (v) => console.log('state.count has changed to', v))
+ */
+export const subscribeKey = <T extends object>(
+  proxyObject: T,
+  key: keyof T,
+  callback: (value: T[typeof key]) => void
+) => {
+  let prevValue = proxyObject[key]
+  return subscribe(proxyObject, () => {
+    const nextValue = proxyObject[key]
+    if (!Object.is(prevValue, nextValue)) {
+      callback((prevValue = nextValue))
+    }
+  })
 }
