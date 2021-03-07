2f695e38af67daca5c6c76acb14b0b79c3066eb4
daishi <daishi@axlight.com>
2021-03-07 11:46:31 +0900

    feat: valtio/macro

---
+++ src/macro.ts
@@ -0,0 +1,39 @@
+import { NodePath } from '@babel/traverse'
+import * as t from '@babel/types'
+import { createMacro, MacroError } from 'babel-plugin-macros'
+// @ts-ignore
+import { addNamed } from '@babel/helper-module-imports'
+
+const macro = ({ references }: any) => {
+  references.bindProxy?.forEach((path: NodePath) => {
+    const hook = addNamed(path, 'useProxy', 'valtio')
+    const proxy = (path.parentPath.get('arguments.0') as any).node
+    if (!t.isIdentifier(proxy)) throw new MacroError('no proxy object')
+    const snap = t.identifier(`valtio_macro_snap_${proxy.name}`)
+    path.parentPath.parentPath.replaceWith(
+      t.variableDeclaration('const', [
+        t.variableDeclarator(snap, t.callExpression(hook, [proxy])),
+      ])
+    )
+    let inRender = true
+    path.parentPath.getFunctionParent()?.traverse({
+      Identifier(p) {
+        if (inRender && p.node !== proxy && p.node.name === proxy.name) {
+          p.node.name = snap.name
+        }
+      },
+      Function: {
+        enter() {
+          inRender = false
+        },
+        exit() {
+          inRender = true
+        },
+      },
+    })
+  })
+}
+
+export declare function bindProxy<T extends object>(proxyObject: T): T
+
+export default createMacro(macro, { configName: 'valtio' })
---
+++ tests/macro.test.ts
@@ -0,0 +1,26 @@
+import pluginTester from 'babel-plugin-tester'
+import plugin from 'babel-plugin-macros'
+
+pluginTester({
+  pluginName: 'valtio/macro',
+  plugin,
+  snapshot: true,
+  babelOptions: {
+    filename: __filename,
+    parserOpts: { plugins: ['jsx'] },
+  },
+  tests: [
+    `
+import { bindProxy } from '../dist/macro'
+
+const Component = () => {
+  bindProxy(state)
+  return (
+    <div>
+      {state.count} <button onClick={() => ++state.count}>inc</button>
+    </div>
+  )
+}
+    `,
+  ],
+})
