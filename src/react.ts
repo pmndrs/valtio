b1c9253875c0e8ebe9dfa920a4413ef73a918a6f
daishi <daishi@axlight.com>
2021-03-31 11:57:37 +0900

    fix: better ssr detection

--- src/react.ts
+++ src/react.ts
@@ -19,7 +19,8 @@ import type { NonPromise } from './vanilla'
 
 const isSSR =
   typeof window === 'undefined' ||
-  /ServerSideRendering/.test(window.navigator && window.navigator.userAgent)
+  typeof window.navigator === 'undefined' ||
+  /ServerSideRendering/.test(window.navigator.userAgent)
 
 const useIsomorphicLayoutEffect = isSSR ? useEffect : useLayoutEffect
 
