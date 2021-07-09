87ae5879a062f1c8bf1355e195043ee6ef46d626
daishi <daishi@axlight.com>
2021-07-09 23:17:55 +0900

    update dependencies and fix type errors

--- src/macro.ts
+++ src/macro.ts
@@ -7,16 +7,16 @@ import { addNamed } from '@babel/helper-module-imports'
 const macro = ({ references }: any) => {
   references.useProxy?.forEach((path: NodePath) => {
     const hook = addNamed(path, 'useSnapshot', 'valtio')
-    const proxy = (path.parentPath.get('arguments.0') as any).node
+    const proxy = (path.parentPath?.get('arguments.0') as any)?.node
     if (!t.isIdentifier(proxy)) throw new MacroError('no proxy object')
     const snap = t.identifier(`valtio_macro_snap_${proxy.name}`)
-    path.parentPath.parentPath.replaceWith(
+    path.parentPath?.parentPath?.replaceWith(
       t.variableDeclaration('const', [
         t.variableDeclarator(snap, t.callExpression(hook, [proxy])),
       ])
     )
     let inFunction = 0
-    path.parentPath.getFunctionParent()?.traverse({
+    path.parentPath?.getFunctionParent()?.traverse({
       Identifier(p) {
         if (
           inFunction === 0 && // in render
