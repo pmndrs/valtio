2dc991e6a745a7bc1bf7ead061b8b835c40b2156
daishi <daishi@axlight.com>
2021-07-31 12:34:08 +0900

    change utils index order

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export { subscribeKey } from './utils/subscribeKey'
+export { watch } from './utils/watch'
 export { devtools } from './utils/devtools'
 export { addComputed } from './utils/addComputed'
 export { proxyWithComputed } from './utils/proxyWithComputed'
-export { watch } from './utils/watch'
 export { proxyWithHistory } from './utils/proxyWithHistory'
