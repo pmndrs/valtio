0771ee7ef162d345ba013453cb7709c690d49de3
daishi <daishi@axlight.com>
2021-07-31 20:16:22 +0900

    wip: derive util

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 export { subscribeKey } from './utils/subscribeKey'
 export { watch } from './utils/watch'
 export { devtools } from './utils/devtools'
+export { derive } from './utils/derive'
 export { addComputed } from './utils/addComputed'
 export { proxyWithComputed } from './utils/proxyWithComputed'
 export { proxyWithHistory } from './utils/proxyWithHistory'
---
+++ src/utils/derive.ts
@@ -0,0 +1,106 @@
+import { proxy, subscribe } from '../vanilla'
+
+type DeriveGet = <T extends object>(value: T) => T
+
+/**
+ * derive
+ *
+ * This creates derived properties and attatches them
+ * to a new proxy object or an existing proxy object.
+ *
+ * @example
+ * import { proxy } from 'valtio'
+ * import { derive } from 'valtio/utils'
+ *
+ * const state = proxy({
+ *   count: 1,
+ * })
+ *
+ * const derived = derive({
+ *   doubled: (get) => get(state).count * 2,
+ * })
+ *
+ * derive({
+ *   tripled: (get) => get(state).count * 3,
+ * }, {
+ *   proxy: state,
+ * })
+ */
+export const derive = <T extends object, U extends object>(
+  derivedFns: {
+    [K in keyof U]: (get: DeriveGet) => U[K]
+  },
+  options?: {
+    proxy?: T
+    sync?: boolean
+    cleanupToken?: { cleanup?: () => void }
+  }
+) => {
+  const proxyObject = (options?.proxy || proxy({})) as U
+  const notifyInSync = options?.sync
+  const subscriptions = new Map<
+    object,
+    [callbackMap: Map<keyof U, () => void>, unsubscribe: () => void]
+  >()
+  if (options?.cleanupToken) {
+    options.cleanupToken.cleanup = () => {
+      // TODO
+    }
+  }
+  const addSubscription = (p: object, key: keyof U, callback: () => void) => {
+    const subscription = subscriptions.get(p)
+    if (subscription) {
+      subscription[0].set(key, callback)
+    } else {
+      const unsubscribe = subscribe(
+        p,
+        () => {
+          subscriptions.get(p)?.[0].forEach((cb) => {
+            cb()
+          })
+        },
+        notifyInSync
+      )
+      subscriptions.set(p, [new Map([[key, callback]]), unsubscribe])
+    }
+  }
+  const removeSubscription = (p: object, key: keyof U) => {
+    const subscription = subscriptions.get(p)
+    if (subscription) {
+      subscription[0].delete(key)
+      if (!subscription[0].size) {
+        subscription[1]()
+        subscriptions.delete(p)
+      }
+    }
+  }
+  ;(Object.keys(derivedFns) as (keyof U)[]).forEach((key) => {
+    if (Object.getOwnPropertyDescriptor(proxyObject, key)) {
+      throw new Error('object property already defined')
+    }
+    const fn = derivedFns[key]
+    let lastDependencies = new Set<object>()
+    const evaluate = () => {
+      // FIXME should avoid duplicated evaluation
+      const dependencies = new Set<object>()
+      const get = <P extends object>(p: P) => {
+        dependencies.add(p)
+        return p
+      }
+      proxyObject[key] = fn(get)
+      dependencies.forEach((p) => {
+        if (!lastDependencies.has(p)) {
+          addSubscription(p, key, evaluate)
+        }
+      })
+      lastDependencies.forEach((p) => {
+        if (!dependencies.has(p)) {
+          removeSubscription(p, key)
+        }
+      })
+      lastDependencies = dependencies
+    }
+    evaluate()
+  })
+  return proxyObject as T & U
+}
