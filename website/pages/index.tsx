9987884dc1c5120c5439395df7787669ec9913bb
Akash <aakash@hey.com>
2022-01-08 21:57:33 +0530

    init next app

---
+++ website/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-full grid place-items-center">
+      <h1 className="font-extrabold text-3xl">Yo</h1>
+    </div>
+  );
+};
+
+export default Home;
