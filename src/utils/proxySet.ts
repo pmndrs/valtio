2392eb132f3dcdeb7cdc451919fcc6dbc85c27f3
Fadi Khadra <fdkhadra@gmail.com>
2022-01-14 14:59:08 +0100

    add doc comment

--- src/utils/proxySet.ts
+++ src/utils/proxySet.ts
@@ -7,6 +7,21 @@ type InternalProxySet<T> = Set<T> & {
   hasProxy(value: T): boolean
 }
 
+/**
+ * proxySet
+ *
+ * This is to create a proxy which mimic the native Set behavior.
+ * The API is the same as Set API
+ *
+ * @example
+ * import { proxySet } from 'valtio/utils'
+ * const state = proxySet([1,2,3])
+ * //can be used inside a proxy as well
+ * const state = proxy({
+ *   count: 1,
+ *   set: proxySet()
+ * })
+ */
 export const proxySet = <T>(initialValues: Iterable<T> | null = []): Set<T> => {
   const set: InternalProxySet<T> = proxy({
     data: Array.from(new Set(initialValues)),
