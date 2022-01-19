589f62f8a5bb8590081cf220545268eb368b8bf4
Fadi Khadra <fdkhadra@gmail.com>
2022-01-19 20:50:04 +0100

    update feature parity test suite

--- tests/proxyMap.test.tsx
+++ tests/proxyMap.test.tsx
@@ -1,5 +1,4 @@
-import { proxySet } from 'valtio/utils'
-import { proxyMap } from 'valtio/utils/proxyMap'
+import { proxySet, proxyMap } from 'valtio/utils'
 
 const consoleError = console.error
 beforeEach(() => {
@@ -18,62 +17,112 @@ afterEach(() => {
   console.error = consoleError
 })
 
-it('features parity with native Map', () => {
-  // contains all possible keys and values types
-  const initialValue = [
-    [-10, 1],
-    ['hello', 'world'],
-    [Symbol(), Symbol()],
-    [false, true],
-    [[10], [1, 2, 3]],
-    [{}, { id: 'somehting', field: null }],
-    [NaN, NaN],
-    [() => {}, () => {}],
-    [new ArrayBuffer(8), new ArrayBuffer(8)],
-    [proxySet([{}]), proxySet([{}])],
-  ]
-  const map = proxyMap(initialValue as any)
-  const nativeMap = new Map(initialValue as any)
+const initialValues = [
+  {
+    name: 'number',
+    value: [-10, 1],
+  },
+  {
+    name: 'string',
+    value: ['hello', 'world'],
+  },
+  {
+    name: 'Symbol',
+    value: [Symbol(), Symbol()],
+  },
+  {
+    name: 'boolean',
+    value: [false, true],
+  },
+  {
+    name: 'array',
+    value: [
+      [10, 'hello'],
+      [1, 2, 3, 'x', 'w'],
+    ],
+  },
+  {
+    name: 'object',
+    value: [{}, { id: 'somehting', field: null }],
+  },
+  {
+    name: 'null',
+    value: [null, [null, 1]],
+  },
+  {
+    name: 'function',
+    value: [() => {}, () => {}],
+  },
+  {
+    name: 'array buffer',
+    value: [new ArrayBuffer(8), new ArrayBuffer(8)],
+  },
+  {
+    name: 'Set',
+    value: [new Set(), new Set(['x', 'y', 'z'])],
+  },
+  {
+    name: 'Map',
+    value: [new Map(), new Map([['key', 'value']])],
+  },
+  {
+    name: 'proxySet',
+    value: [proxySet([{}]), proxySet([{}, Symbol()])],
+  },
+]
 
-  // check for Symbol.toStringTag / toString
-  expect(`${map}`).toBe(`${nativeMap}`)
+describe('features parity with native Map', () => {
+  initialValues.forEach(({ name, value }) => {
+    it(`Support Map operations on ${name}`, () => {
+      const map = proxyMap([value as any])
+      const nativeMap = new Map([value as any])
 
-  const expectOutputToMatch = () => {
-    expect(map.size).toStrictEqual(nativeMap.size)
-    expect(Array.from(map.values())).toStrictEqual(
-      Array.from(nativeMap.values())
-    )
-    expect(Array.from(map.keys())).toStrictEqual(Array.from(nativeMap.keys()))
-    expect(Array.from(map.entries())).toStrictEqual(
-      Array.from(nativeMap.entries())
-    )
-    expect(JSON.stringify(map)).toStrictEqual(JSON.stringify(nativeMap))
+      // check for Symbol.toStringTag / toString
+      expect(`${map}`).toBe(`${nativeMap}`)
 
-    // cover loops
-    const handleForEach = jest.fn()
-    const handleForOf = jest.fn()
+      const expectOutputToMatch = () => {
+        expect(map.size).toStrictEqual(nativeMap.size)
+        expect(Array.from(map.values())).toStrictEqual(
+          Array.from(nativeMap.values())
+        )
+        expect(Array.from(map.keys())).toStrictEqual(
+          Array.from(nativeMap.keys())
+        )
+        expect(Array.from(map.entries())).toStrictEqual(
+          Array.from(nativeMap.entries())
+        )
+        expect(JSON.stringify(map)).toStrictEqual(JSON.stringify(nativeMap))
 
-    map.forEach(handleForEach)
-    expect(handleForEach).toHaveBeenCalledTimes(map.size)
+        // cover loops
+        const handleForEach = jest.fn()
+        const handleForOf = jest.fn()
 
-    for (const _ of map) {
-      handleForOf()
-    }
+        map.forEach(handleForEach)
+        expect(handleForEach).toHaveBeenCalledTimes(map.size)
 
-    expect(handleForOf).toHaveBeenCalledTimes(map.size)
-  }
+        for (const _ of map) {
+          handleForOf()
+        }
 
-  expectOutputToMatch()
+        expect(handleForOf).toHaveBeenCalledTimes(map.size)
+      }
 
-  const randomKey = Array.from(map.keys())[Math.floor(Math.random() * map.size)]
+      expectOutputToMatch()
 
-  expect(map.has(randomKey)).toBe(nativeMap.has(randomKey))
+      const [firstElementFromMap] = map
+      const [firstElementFromNativeMap] = nativeMap
+      const keyFromMap = firstElementFromMap![0]
+      const keyFromNativeMap = firstElementFromNativeMap![0]
 
-  map.delete(randomKey)
-  nativeMap.delete(randomKey)
-  expectOutputToMatch()
+      expect(map.has(keyFromMap)).toBe(nativeMap.has(keyFromNativeMap))
 
-  map.set('newKey', {})
-  nativeMap.set('newKey', {})
-  expectOutputToMatch()
+      map.delete(keyFromMap)
+      nativeMap.delete(keyFromNativeMap)
+      expectOutputToMatch()
+
+      map.set('newKey', {})
+      nativeMap.set('newKey', {})
+      expectOutputToMatch()
+    })
+  })
 })
