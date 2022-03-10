dcfca4c5207e0719c090939154bc3e375a21372e
M. Bagher Abiat <zorofight94@gmail.com>
2022-03-10 18:08:25 +0330

    Update src/utils/devtools.ts
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/utils/devtools.ts
+++ src/utils/devtools.ts
@@ -2,7 +2,7 @@ import { snapshot, subscribe } from '../vanilla'
 import type {} from '@redux-devtools/extension'
 
 // FIXME https://github.com/reduxjs/redux-devtools/issues/1097
-export type Message = {
+type Message = {
   type: string
   payload?: any
   state?: any
