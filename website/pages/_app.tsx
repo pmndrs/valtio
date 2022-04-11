5ef8656ab8ca7f6c20251a2e4b728032c872fa63
Richard Van Camp <richard.van.camp@wolt.com>
2022-04-11 08:31:09 +0300

    chore: website landing page with animation duration state example (#421)
    * chore: website landing page with animation duration state example
    
    * chore: simplify state in landing page ex

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 
 import "~/styles/tailwind.css";
 import "~/styles/prism-theme.css";
+import "~/styles/landing-page.css";
 
 type NextPageWithLayout = NextPage & {
   layoutProps: {
