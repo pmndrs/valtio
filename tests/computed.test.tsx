137850df0d1a805def4fb03589dacb00216bcaa2
daishi <daishi@axlight.com>
2022-04-17 21:48:28 +0900

    remove extra waitFor import in tests

--- tests/computed.test.tsx
+++ tests/computed.test.tsx
@@ -1,5 +1,5 @@
 import { StrictMode, Suspense } from 'react'
-import { fireEvent, render, waitFor } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { proxy, snapshot, subscribe, useSnapshot } from 'valtio'
 import { addComputed, proxyWithComputed } from 'valtio/utils'
 
