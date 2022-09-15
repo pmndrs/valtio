6e58c9d0edc24a2507c69bc2aaf52b03c13786d5
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2022-09-16 05:57:40 +0700

    fix(docs): website title (#550)
    * fix: default title
    
    * fix: invalid docs url

--- website/components/layouts/Header/Header.tsx
+++ website/components/layouts/Header/Header.tsx
@@ -3,6 +3,7 @@ import Router from "next/router";
 import { Dialog } from "@headlessui/react";
 import React, { useEffect, useState } from "react";
 import clsx from "clsx";
+import SEO from "~/components/SEO";
 // import { ThemeSelect, ThemeToggle } from "./ThemeToggle";
 // import ToggleTheme from "~/components/ToggleTheme";
 
@@ -94,7 +95,7 @@ export function NavItems() {
   return (
     <>
       <li>
-        <Link href="/docs/basic/proxy">
+        <Link href="/docs/introduction/getting-started">
           <a className="hover:text-sky-500 dark:hover:text-sky-400">Docs</a>
         </Link>
       </li>
@@ -139,6 +140,7 @@ export default function Header({
 
   return (
     <>
+    <SEO title={title} />
       <div
         className={clsx(
           "sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06]",
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,10 +1,12 @@
 import { AnimatedShapes } from "~/components/LandingPage/AnimatedShapes";
 import { GettingStarted } from "~/components/LandingPage/GettingStarted";
 import { CodeExample } from "~/components/LandingPage/CodeExample";
+import SEO from "~/components/SEO";
 
 const Home = () => {
   return (
     <div className="min-h-screen landing-page-container">
+      <SEO />
       <CodeExample />
       <AnimatedShapes />
       <GettingStarted className="large-screen" />
