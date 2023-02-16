60e6ab2cf74fbba877a7ea52c10da1ca161512f1
Nick K <nick.kruchinin@gmail.com>
2023-02-16 17:26:43 +0300

    Add pnpm and npm lockfiles to .gitignore. Add tests for Snapshot type.

---
+++ tests/typings/Snapshot.test-d.ts
@@ -0,0 +1,95 @@
+import type { TypeEqual } from 'ts-expect'
+import { expectType } from 'ts-expect'
+import { INTERNAL_Snapshot as Snapshot } from '../../src/vanilla'
+
+// Converts object properties to readonly
+expectType<
+  TypeEqual<
+    Snapshot<{
+      string: string
+      number: number
+      null: null
+      undefined: undefined
+      bool: boolean
+      someFunction(): number
+      ref: {
+        $$valtioRef: true
+      }
+    }>,
+    {
+      readonly string: string
+      readonly number: number
+      readonly null: null
+      readonly undefined: undefined
+      readonly bool: boolean
+      readonly someFunction: () => number
+      readonly ref: {
+        $$valtioRef: true
+      }
+    }
+  >
+>(true)
+
+// Infers Promise result from value
+expectType<
+  TypeEqual<
+    Snapshot<{ promise: Promise<string> }>,
+    { readonly promise: string }
+  >
+>(true)
+
+// Converts arrays to readonly arrays
+expectType<TypeEqual<Snapshot<number[]>, readonly number[]>>(true)
+
+// Keeps builtin objects as-is. Date, Map and Set as the example.
+expectType<
+  TypeEqual<
+    Snapshot<{ date: Date; map: Map<string, unknown>; set: Set<string> }>,
+    {
+      readonly date: Date
+      readonly map: Map<string, unknown>
+      readonly set: Set<string>
+    }
+  >
+>(true)
+
+// Converts collections to reaonly
+expectType<
+  TypeEqual<Snapshot<{ key: string }[]>, readonly { readonly key: string }[]>
+>(true)
+
+// Convets object properties to readonly recursively
+expectType<
+  TypeEqual<
+    Snapshot<{
+      prevPage: number | null
+      nextPage: number | null
+      rows: number
+      items: {
+        title: string
+        details: string | null
+        createdAt: Date
+        updatedAt: Date
+        completions: {
+          details: string
+          completed: boolean
+        }[]
+      }[]
+    }>,
+    {
+      readonly prevPage: number | null
+      readonly nextPage: number | null
+      readonly rows: number
+      readonly items: readonly {
+        readonly title: string
+        readonly details: string | null
+        readonly createdAt: Date
+        readonly updatedAt: Date
+        readonly completions: readonly {
+          readonly details: string
+          readonly completed: boolean
+        }[]
+      }[]
+    }
+  >
+>(true)
