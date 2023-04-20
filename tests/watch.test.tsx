883c5b38ec67575d8b0e716e10a22d1ae5193511
M. Bagher Abiat <zorofight94@gmail.com>
2023-04-20 17:42:27 +0330

    update

--- tests/watch.test.tsx
+++ tests/watch.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from 'vitest'
 import { watch } from 'valtio/utils'
-import { proxy } from 'valtio/vanilla'
+import { proxy } from 'valtio'
 
 describe('watch', () => {
   it('should re-run for individual proxy updates', async () => {
