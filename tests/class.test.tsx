63b9cf647657d366fb219bca5993f72e9d82b268
Yehyoung Kang <keepyourhonor+github@gmail.com>
2023-07-24 11:58:30 +0900

    Revert "feat(vanilla): Support class fields (defineProperty) (#760)"
    This reverts commit c0bda5afd2780fb929656a87bcc9e16bcf189fd8.
    
    Due to what I presume is a bug in the Hermes engine, React Native
    projects cannot use Valtio 1.11.0. Let's revert #760 to support existing
    React Native users.

--- tests/class.test.tsx
+++ tests/class.test.tsx
@@ -355,31 +355,3 @@ it('no extra re-renders with getters', async () => {
     getByText('sum: 2 (2)')
   })
 })
-
-it('support class fields (defineProperty semantics)', async () => {
-  class Base {
-    constructor() {
-      return proxy(this)
-    }
-  }
-  class CountClass extends Base {
-    counter = { count: 0 }
-  }
-  const obj = new CountClass()
-
-  const Counter = () => {
-    const snap = useSnapshot(obj)
-    return <div>count: {snap.counter.count}</div>
-  }
-
-  const { findByText } = render(
-    <StrictMode>
-      <Counter />
-    </StrictMode>
-  )
-
-  await findByText('count: 0')
-
-  obj.counter.count = 1
-  await findByText('count: 1')
-})
