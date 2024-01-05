89c96b139d987d5cc4c291ff4d7762ff61955ac2
Layton Whiteley <layton.whiteley@gmail.com>
2024-01-05 20:24:20 +0100

    docs: update proxyWithHistory example with new package

--- examples/editor-proxyWithHistory/src/App.tsx
+++ examples/editor-proxyWithHistory/src/App.tsx
@@ -1,22 +1,29 @@
 import './styles.css'
 import React from 'react'
 import { useSnapshot } from 'valtio'
-import { proxyWithHistory } from 'valtio/utils'
+import { proxyWithHistory } from 'valtio-history'
 
 const textProxy = proxyWithHistory({
-  text: 'Add some test to this initial value and then undo/redo',
+  text: 'Add some text to this initial value and then undo/redo',
 })
-const update = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
-  (textProxy.value.text = e.target.value)
+
+const update = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
+  (textProxy.value.text = event.target.value)
 
 export default function App() {
-  const { value, undo, redo, history, canUndo, canRedo } =
+  const { value, undo, redo, history, canUndo, canRedo, getCurrentChangeDate } =
     useSnapshot(textProxy)
 
   return (
     <div className="App">
       <h2>Editor with history</h2>
-      <small>{history.index} changes</small>
+      <div className="info">
+        <span>
+          change {history.index + 1} / {history.nodes.length}
+        </span>
+        <span>|</span>
+        <span>{getCurrentChangeDate().toISOString()}</span>
+      </div>
       <div className="editor">
         <textarea value={value.text} rows={4} onChange={update} />
       </div>
