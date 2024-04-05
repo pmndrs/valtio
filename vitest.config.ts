94be18b97039e3bad67b5e9e70b1bfcd259f5432
daishi <daishi@axlight.com>
2024-04-06 00:12:30 +0900

    hack node_modules

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,12 +18,5 @@ export default defineConfig({
     },
     environment: 'jsdom',
     dir: 'tests',
-    deps: {
-      optimizer: {
-        web: {
-          include: ['derive-valtio'],
-        },
-      },
-    },
   },
 })
