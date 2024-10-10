99dd2351954806daaff0b5e977cd49a0b8409417
Michael Sweeney <overthemike@gmail.com>
2024-10-10 02:22:49 -0700

    pushing up for testing

--- examples/test/src/App.tsx
+++ examples/test/src/App.tsx
@@ -1,5 +1,35 @@
+import { useEffect } from 'react'
 import { useSnapshot, proxy, subscribe } from '../../../dist/esm/index.mjs'
 import { proxyMap } from '../../../dist/esm/utils.mjs'
+
+const state = proxyMap()
+const k1 = {}
+state.set(k1, 'hello')
+
+const App = () => {
+  const snap = useSnapshot(state)
+  const handleClick = () => {
+    state.delete(k1)
+    state.set('foo', 'asdf')
+  }
+
+  console.log('rendering')
+
+  useEffect(() => {
+    console.log(snap)
+  }, [snap])
+  console.log(snap.get(k1) === k1)
+  return (
+    <>
+      <p>{snap.get(k1)}</p>
+      <p>{snap.has('foo') ? 'foo' : 'no foo'}</p>
+      <button onClick={handleClick}>Click Me</button>
+      <div>{Math.random()}</div>
+    </>
+  )
+}
+
+export default App
 
 // const state = proxyMap([
 //   ['first', 0],
@@ -7,14 +37,11 @@ import { proxyMap } from '../../../dist/esm/utils.mjs'
 //   ['second', 2],
 //   ['foo', 3],
 // ])
-// let i = 0
 // function App() {
 //   const snap = useSnapshot(state)
-//   const entries = snap.entries()
 
 //   const handleClick = () => {
-//     state.set('first', i++)
-//     console.log(entries.next())
+//     state.set('first', 10)
 //   }
 
 //   return (
@@ -31,53 +58,53 @@ import { proxyMap } from '../../../dist/esm/utils.mjs'
 
 // export default App
 
-const state = proxyMap([['test', 'test']])
+// const state = proxyMap([['test', 'test']])
 
-setTimeout(() => {
-  state.set('asdf', 'bar')
-}, 5000)
+// setTimeout(() => {
+//   state.set('asdf', 'bar')
+// }, 5000)
 
-setTimeout(() => {
-  state.set('bat', 'foo')
-}, 7000)
+// setTimeout(() => {
+//   state.set('bat', 'foo')
+// }, 7000)
 
-setTimeout(() => {
-  state.set('foo', 'bar')
-  state.set('test', 'test')
-}, 9000)
+// setTimeout(() => {
+//   state.set('foo', 'bar')
+//   state.set('test', 'test')
+// }, 9000)
 
-const Comp1 = () => {
-  const snap = useSnapshot(state)
+// const Comp1 = () => {
+//   const snap = useSnapshot(state)
 
-  console.log('COMP1 RENDER')
+//   console.log('COMP1 RENDER')
 
-  return <p>{state.has('foo') ? snap.get('foo') : ''}</p>
-}
+//   return <p>{state.has('foo') ? snap.get('foo') : ''}</p>
+// }
 
-const Comp2 = () => {
-  const snap = useSnapshot(state)
+// const Comp2 = () => {
+//   const snap = useSnapshot(state)
 
-  console.log('COMP2 RENDER')
+//   console.log('COMP2 RENDER')
 
-  return <p>{state.has('bar') ? snap.get('bar') : ''}</p>
-}
+//   return <p>{state.has('bar') ? snap.get('bar') : ''}</p>
+// }
 
-const Comp3 = () => {
-  const snap = useSnapshot(state)
+// const Comp3 = () => {
+//   const snap = useSnapshot(state)
 
-  console.log('COMP3 RENDER')
+//   console.log('COMP3 RENDER')
 
-  return <p>{state.has('test') ? snap.get('test') : ''}</p>
-}
+//   return <p>{state.has('test') ? snap.get('test') : ''}</p>
+// }
 
-const App = () => {
-  return (
-    <>
-      <Comp1 />
-      <Comp2 />
-      <Comp3 />
-    </>
-  )
-}
+// const App = () => {
+//   return (
+//     <>
+//       <Comp1 />
+//       <Comp2 />
+//       <Comp3 />
+//     </>
+//   )
+// }
 
-export default App
+// export default App
