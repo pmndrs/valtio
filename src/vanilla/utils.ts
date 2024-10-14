d2aad5364e21c20b523b7110bce4d94a532f1431
daishi <daishi@axlight.com>
2024-10-14 13:46:43 +0900

    revert default

--- src/vanilla/utils.ts
+++ src/vanilla/utils.ts
@@ -3,7 +3,7 @@ export { watch } from './utils/watch.ts'
 export { devtools } from './utils/devtools.ts'
 export { deepClone } from './utils/deepClone.ts'
 export { proxySet } from './utils/proxySet.ts'
-export { proxyMap } from './utils/proxyMap-old.ts'
+export { proxyMap } from './utils/proxyMap-indexMap-keyvals.ts'
 
 export { proxyMap as newProxyMap } from './utils/proxyMap-indexMap-filled.ts'
 export { proxyMap as newProxyMapKeyVals } from './utils/proxyMap-indexMap-keyvals.ts'
