e2b3f1e17bbcb948d381a81729d461321bb487c1
Michael Sweeney <overthemike@gmail.com>
2024-10-14 03:21:25 -0700

    added snap test to proxySet

--- src/vanilla/utils/test.ts
+++ src/vanilla/utils/test.ts
@@ -1,13 +1,13 @@
 import { snapshot } from '../../vanilla.ts'
 import { proxyMap } from './proxyMap-indexMap-keyval.ts'
+import { proxySet } from './proxySet.ts'
 
-const state = proxyMap([['key1', 'val1']])
+const state = proxySet(['val1', 'val2'])
 const snap1 = snapshot(state)
-console.log(snap1.has('key1'), 'true')
-console.log(snap1.get('key1'), 'val1')
-state.delete('key1')
+console.log(snap1.has('val1'), 'true')
+state.delete('val2')
 const snap2 = snapshot(state)
-console.log(snap1.has('key1'), 'true')
-console.log(snap1.get('key1'), 'val1')
-console.log(snap2.has('key1'), 'false')
-console.log(snap2.get('key1'), 'undefined')
+console.log(snap1.has('val1'), 'true')
+console.log(snap1.has('val2'), 'true')
+console.log(snap2.has('val1'), 'true')
+console.log(snap2.has('val2'), 'false')
