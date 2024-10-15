ffe6b72046024d55f1b1ffb63879103aaec48cad
Michael Sweeney <overthemike@gmail.com>
2024-10-14 17:51:06 -0700

    removed test file

--- src/vanilla/utils/test.ts
+++
@@ -1,9 +0,0 @@
-import { proxy, snapshot } from '../../vanilla.ts'
-import { proxyMap } from './proxyMap.ts'
-
-const p = proxyMap()
-const k = proxy({})
-p.set(k, 1)
-const snap = snapshot(p)
-console.log(snap.get(k), 1)
-console.log(snap.get(snapshot(k)), 1)
