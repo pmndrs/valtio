2a96acf15de0c2d900e9d5b157aaacc345c22993
Michael Sweeney <overthemike@gmail.com>
2024-10-15 20:00:15 -0700

    added jsdoc comments for proxyMap and proxySet (#974)
    * added jsdoc comments for proxyMap and proxySet
    
    * fixed spacing in jsdoc comment

--- src/vanilla/utils/proxyMap.ts
+++ src/vanilla/utils/proxyMap.ts
@@ -9,6 +9,34 @@ type InternalProxyObject<K, V> = Map<K, V> & {
   toJSON: () => Map<K, V>
 }
 
+/**
+ * proxyMap
+ *
+ * This is to create a proxy which mimic the native Map behavior.
+ * The API is the same as Map API
+ *
+ * @example
+ * import { proxyMap } from 'valtio/utils'
+ * const state = proxyMap([["key", "value"]])
+ *
+ * // can be used inside a proxy as well
+ * const state = proxy({
+ *   count: 1,
+ *   map: proxyMap()
+ * })
+ *
+ * // When using an object as a key, you can wrap it with `ref` so it's not proxied
+ * // this is useful if you want to preserve the key equality
+ * import { ref } from 'valtio'
+ *
+ * const key = ref({})
+ * state.set(key, "value")
+ * state.get(key) //value
+ *
+ * const key = {}
+ * state.set(key, "value")
+ * state.get(key) //undefined
+ */
 export function proxyMap<K, V>(entries?: Iterable<[K, V]> | undefined | null) {
   const initialData: Array<V> = []
   let initialIndex = 0
--- src/vanilla/utils/proxySet.ts
+++ src/vanilla/utils/proxySet.ts
@@ -16,6 +16,21 @@ type InternalProxySet<T> = Set<T> & {
   union: (other: Set<T>) => Set<T>
 }
 
+/**
+ * proxySet
+ *
+ * This is to create a proxy which mimic the native Set behavior.
+ * The API is the same as Set API
+ *
+ * @example
+ * import { proxySet } from 'valtio/utils'
+ * const state = proxySet([1,2,3])
+ * // can be used inside a proxy as well
+ * const state = proxy({
+ *   count: 1,
+ *   set: proxySet()
+ * })
+ */
 export function proxySet<T>(initialValues?: Iterable<T> | null) {
   const initialData: T[] = []
   const indexMap = new Map<T, number>()
