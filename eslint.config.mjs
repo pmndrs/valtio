f5f680984978f5ce401d1348c9bbc3c8f5eaa659
Wonsuk Choi <sukvvon@gmail.com>
2024-12-29 23:48:25 +0900

    chore(eslint): add 'dist/**', 'examples/**', 'website/**' in 'ignores'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  {
+    ignores: ['dist/**', 'examples/**', 'website/**'],
+  },
   eslint.configs.recommended,
   tseslint.configs.recommended,
   react.configs.flat['jsx-runtime'],
