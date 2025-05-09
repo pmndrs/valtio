d7840e1d5f234e98d5f94158dafaad9eafcba908
daishi <daishi@axlight.com>
2025-05-09 22:44:53 +0900

    chore: fix eslint warnings

--- tests/class.test.tsx
+++ tests/class.test.tsx
@@ -8,7 +8,6 @@ const useCommitCount = () => {
   useEffect(() => {
     commitCountRef.current += 1
   })
-  // eslint-disable-next-line react-compiler/react-compiler
   return commitCountRef.current
 }
 
