4f044e068c690f6363d5dfbfa9a1016c30cfa221
daishi <daishi@axlight.com>
2025-08-16 22:47:37 +0900

    test: add more memory leak cases

--- tests/memoryleaks.test.ts
+++ tests/memoryleaks.test.ts
@@ -1,6 +1,6 @@
 import LeakDetector from 'jest-leak-detector'
 import { describe, expect, it } from 'vitest'
-import { proxy } from 'valtio'
+import { proxy, subscribe } from 'valtio'
 
 describe('no memory leaks with proxy', () => {
   it('empty object', async () => {
@@ -38,6 +38,116 @@ describe('no memory leaks with proxy', () => {
       const state = proxy({ child })
       detector = new LeakDetector(state)
     })()
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('object cycle (level 1)', async () => {
+    let detector: LeakDetector
+    ;(() => {
+      const state = proxy({} as { child?: unknown })
+      state.child = state
+      detector = new LeakDetector(state)
+    })()
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('object cycle (level 2)', async () => {
+    let detector: LeakDetector
+    ;(() => {
+      const state = proxy({ child: {} as { child?: unknown } })
+      state.child.child = state
+      detector = new LeakDetector(state)
+    })()
+    expect(await detector.isLeaking()).toBe(false)
+  })
+})
+
+describe('no memory leaks with proxy with subscription', () => {
+  it('empty object', async () => {
+    let detector: LeakDetector
+    let unsub: (() => void) | undefined
+    ;(() => {
+      const state = proxy({})
+      detector = new LeakDetector(state)
+      unsub = subscribe(state, () => {})
+    })()
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    unsub()
+    unsub = undefined
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('child object', async () => {
+    let detector: LeakDetector
+    let unsub: (() => void) | undefined
+    ;(() => {
+      const state = proxy({ child: {} })
+      detector = new LeakDetector(state)
+      unsub = subscribe(state, () => {})
+    })()
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    unsub()
+    unsub = undefined
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('global child object', async () => {
+    let detector: LeakDetector
+    let unsub: (() => void) | undefined
+    ;(() => {
+      const child = {}
+      const state = proxy({ child })
+      detector = new LeakDetector(state)
+      unsub = subscribe(state, () => {})
+    })()
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    unsub()
+    unsub = undefined
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('global child proxy', async () => {
+    let detector: LeakDetector
+    let unsub: (() => void) | undefined
+    ;(() => {
+      const child = proxy({})
+      const state = proxy({ child })
+      detector = new LeakDetector(state)
+      unsub = subscribe(state, () => {})
+    })()
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    unsub()
+    unsub = undefined
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('object cycle (level 1)', async () => {
+    let detector: LeakDetector
+    let unsub: (() => void) | undefined
+    ;(() => {
+      const state = proxy({} as { child?: unknown })
+      state.child = state
+      detector = new LeakDetector(state)
+      unsub = subscribe(state, () => {})
+    })()
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    unsub()
+    unsub = undefined
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('object cycle (level 2)', async () => {
+    let detector: LeakDetector
+    let unsub: (() => void) | undefined
+    ;(() => {
+      const state = proxy({ child: {} as { child?: unknown } })
+      state.child.child = state
+      detector = new LeakDetector(state)
+      unsub = subscribe(state, () => {})
+    })()
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    unsub()
+    unsub = undefined
     expect(await detector.isLeaking()).toBe(false)
   })
 })
