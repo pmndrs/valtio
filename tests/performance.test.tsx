7ccfdd30bd9f8cf49689972e2ccab98064fa79f4
pilaoda <793493083@qq.com>
2025-08-19 03:48:49 +0800

    clean performance test

--- tests/performance.test.tsx
+++ tests/performance.test.tsx
@@ -4,7 +4,6 @@ import { subscribeKey } from 'valtio/utils'
 
 const DEPTHS = [4, 8, 16, 32, 64, 128, 256]
 const KEYS = [4, 8, 16, 32, 64, 128, 256, 512, 1024]
-// const KEYS = [512, 1024]
 const REPEATS = 5000
 
 const measurePerformance = (
